3535c0d975d1c7ca8db9fe310ae95098ca8fa42d
Debbie <cinemaestro@yahoo.com>
2015-01-30 10:30:49 -0800

    add validation specs and methods for both classes

--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -2,4 +2,9 @@ require('spec_helper')
 
 describe(Band) do
   it { should have_and_belong_to_many(:venues) }
+
+  it ("validates the presence of a band name") do
+    band = Band.new({:band_name => ""})
+    expect(band.save()).to eq(false)
+  end
 end
--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -2,4 +2,9 @@ require('spec_helper')
 
 describe(Venue) do
   it { should have_and_belong_to_many(:bands) }
+
+  it ("validates the presence of a Venue name") do
+    venue = Venue.new({:venue_name => ""})
+    expect(venue.save()).to eq(false)
+  end
 end
